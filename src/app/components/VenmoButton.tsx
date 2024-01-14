1ca7b75a941773e344e3d621650588dd013bab9a
Eric Kelly <github@useric.com>
2024-01-13 16:54:51 -0800

    Hide Venmo button on non-mobile OS

--- src/app/components/VenmoButton.tsx
+++ src/app/components/VenmoButton.tsx
@@ -7,13 +7,15 @@ interface VenmoButtonProps {
     description?: string,
 }
 
+export const isMobileDevice = () => /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
 export default function VenmoButton(props: VenmoButtonProps) {
 
-    return <Button 
+    return isMobileDevice() ? <Button 
         sx={{ backgroundColor: "#008CFF", 
             '&:hover': {
                 backgroundColor: "#0072cf",
           }, }}
         href={getVenmoDeeplink(props.amount, props.description)}
-    ><Image width={48} height={12} alt="Venmo" src="./Venmo_Logo_White_small.png" /></Button>
+    ><Image width={48} height={12} alt="Venmo" src="./Venmo_Logo_White_small.png" /></Button> : null;
 }
