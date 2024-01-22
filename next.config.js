3d7ceead5ab26b935073ebe5467442b3aff4c1c3
Eric Kelly <github@useric.com>
2024-01-21 17:21:10 -0800

    Add offline mode

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  cacheStartUrl: true,
+  cacheOnFrontEndNav: true,
+  reloadOnOnline: false,
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true
   },
-  redirects: async () => [
-    {
-      source: '/',
-      destination: '/home',
-      permanent: true,
-    },
-  ]
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,22 +37,22 @@ export default function RootLayout({
     <DrawerAppBar navItems={routes}>{children}</DrawerAppBar>
   );
 
-  React.useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      // Register a service worker hosted at the root of the
-      // site using the default scope.
-      navigator.serviceWorker.register("/sw.js").then(
-        (registration) => {
-          console.log("Service worker registration succeeded:", registration);
-        },
-        (error) => {
-          console.error(`Service worker registration failed: ${error}`);
-        }
-      );
-    } else {
-      console.error("Service workers are not supported.");
-    }
-  });
+  // React.useEffect(() => {
+  //   if ("serviceWorker" in navigator) {
+  //     // Register a service worker hosted at the root of the
+  //     // site using the default scope.
+  //     navigator.serviceWorker.register("/sw.js").then(
+  //       (registration) => {
+  //         console.log("Service worker registration succeeded:", registration);
+  //       },
+  //       (error) => {
+  //         console.error(`Service worker registration failed: ${error}`);
+  //       }
+  //     );
+  //   } else {
+  //     console.error("Service workers are not supported.");
+  //   }
+  // });
 
   return (
     <html lang="en">
