b92c68d2ceca9ff9bd05ac409b6cf096b09b3413
Eric Kelly <github@useric.com>
2024-01-27 22:04:37 -0800

    Fix identification of mobile

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,3 @@
-
 // Options to determine if we are on a touch-capable device:
 // let isTouch1 = window.matchMedia("(pointer: coarse)").matches;
 // let isTouch2 = navigator.maxTouchPoints > 0;
@@ -6,5 +5,5 @@
 // from: https://stackoverflow.com/questions/73093713/detect-if-the-browser-has-swipe-navigation-gestures-enabled-with-javascript
 
 export const isTouchEnabled = () => {
-    return !!window.ontouchstart;
-}
+  return window.matchMedia("(pointer: coarse)").matches;
+};
